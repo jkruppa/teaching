30381e022ebdf024dc99947f222e4d9c96f507c6
Jochen Kruppa <jkruppa@users.noreply.github.com>
2022-12-13 17:16:01 +0100

    added text

--- Spielweise in R (Level 3)/01_soli_analysis/01_soil_analysis.R
+++ Spielweise in R (Level 3)/01_soli_analysis/01_soil_analysis.R
@@ -6,25 +6,71 @@
 ## load packages, please install first the R package pacman
 ## install.packages("pacman")
 pacman::p_load(tidyverse, readxl, janitor,
-               emmeans, multcomp)
+               emmeans, multcomp, 
+               parameters, effectsize,
+               conflicted)
+conflict_prefer("select", "dplyr")
+conflict_prefer("filter", "dplyr")
 
 ## wir lesen immer nur ein Tabellenblatt ein
-data_tbl <- read_excel("/Users/kruppajo/Documents/GitHub/teaching/Spielweise in R (Level 3)/01_soli_analysis/soil_ph_data.xlsx") %>% 
+data_tbl <- read_excel("soil_ph_data.xlsx") %>% 
   clean_names() %>% 
+  select(-grade) %>% 
   mutate(substrat = as_factor(substrat),
          ph = as_factor(ph))
 
-ggplot(data_tbl, aes(x = substrat, y = diameter, fill = ph)) +
-  geom_boxplot()
+## Abbildung machen
+ggplot(data_tbl, aes(x = substrat, y = freshmatter, fill = ph)) +
+  geom_boxplot() +
+  theme_minimal() +
+  labs(x = "Substratvariante", y = "Frischgewicht in kg/h",
+       fill = "pH-Wert")
 
+## lineares Modell
 fit <- lm(freshmatter ~ substrat + ph + substrat:ph, data = data_tbl)
 
-fit %>%  anova()
+fit %>% 
+  anova() %>% 
+  model_parameters()
+
+fit %>% 
+  eta_squared()
+
+## Paarweise Vergleiche
 
 fit %>% 
   emmeans(~ substrat) %>% 
-  cld(Letters = letters, adjust = "none")
+  cld(Letters = letters, adjust = "none") %>% ## bonferroni
+  arrange(substrat)
+
+fit %>% 
+  emmeans(~ substrat) %>%   
+  contrast(method = "pairwise", adjust = "bonferroni") %>% 
+  as_tibble() %>% 
+  arrange(p.value)
 
 fit %>% 
   emmeans(~ ph) %>% 
   cld(Letters = letters, adjust = "none")
+
+## das ganze mit interaktion
+
+fit %>% 
+  emmeans(~ substrat | ph) %>% 
+  cld(Letters = letters, adjust = "none") ## bonferroni
+
+fit %>% 
+  emmeans(~ substrat | ph) %>%   
+  contrast(method = "pairwise", adjust = "none") %>% 
+  as_tibble() %>% 
+  filter(ph == "high") %>% 
+  arrange(p.value)
+
+
+
+
+
+
+
+
+

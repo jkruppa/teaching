eb0a724ed8faef4d9bd92e834ebd46f3e1692397
Jochen Kruppa <jkruppa@users.noreply.github.com>
2023-03-30 10:17:24 +0200

    added text

--- Grundlagen in R (Level 1)/01_expand_grid/01_expand_grid.R
+++ Grundlagen in R (Level 1)/01_expand_grid/01_expand_grid.R
@@ -4,17 +4,17 @@ design_tbl <- expand_grid(light = 1:3,
                           variety = 1:2,
                           replication = 1:4) %>% 
   mutate(light = factor(light, labels = c("200", "400", "600")),
-         variety = factor(variety, labels = c("A", "B")),
-         id = 1:n()) 
+         variety = factor(variety, labels = c("A", "B"))) 
 
 design_tbl %>% 
-  slice_sample(prop = 1) 
+  slice_sample(prop = 1) %>% 
+  mutate(id = 1:n())
 
 design_tbl %>% 
   write_csv2("experimental_design.csv")  
 
 nrow(design_tbl)
 
-1:24 %>% sample() %>% matrix(nrow = 4, ncol = 6)
+1:12 %>% sample() %>% matrix(nrow = 3, ncol = 4)
 
 

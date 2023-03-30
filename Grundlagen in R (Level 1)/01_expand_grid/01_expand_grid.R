aa1b3fedca31d35a46c090b9d521cbd7423adf43
Jochen Kruppa <jkruppa@users.noreply.github.com>
2023-03-30 10:01:09 +0200

    added text

---
+++ Grundlagen in R (Level 1)/01_expand_grid/01_expand_grid.R
@@ -0,0 +1,20 @@
+pacman::p_load(tidyverse, readxl)
+
+design_tbl <- expand_grid(light = 1:3,
+                          variety = 1:2,
+                          replication = 1:4) %>% 
+  mutate(light = factor(light, labels = c("200", "400", "600")),
+         variety = factor(variety, labels = c("A", "B")),
+         id = 1:n()) 
+
+design_tbl %>% 
+  slice_sample(prop = 1) 
+
+design_tbl %>% 
+  write_csv2("experimental_design.csv")  
+
+nrow(design_tbl)
+
+1:24 %>% sample() %>% matrix(nrow = 4, ncol = 6)
+
+

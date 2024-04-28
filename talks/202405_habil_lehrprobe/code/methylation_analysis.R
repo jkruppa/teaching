17aab02ff1e36311de5f200ded432cf697a7f5dd
Jochen Kruppa <jkruppa@users.noreply.github.com>
2024-04-28 21:35:02 +0200

    added text

--- talks/202405_habil_lehrprobe/code/methylation_analysis.R
+++ talks/202405_habil_lehrprobe/code/methylation_analysis.R
@@ -1,19 +1,28 @@
 ## -----------------------------------------------------------------------------
 ## CAUTION: The installation of the packages will take a bunch (!) of time... 
 
-pacman::p_load(tidyverse, IlluminaHumanMethylation450kanno.ilmn12.hg19,
-               methylationArrayAnalysis)
+pacman::p_load(tidyverse, IlluminaHumanMethylation450kanno.ilmn12.hg19)
 
-## 
+## -----------------------------------------------------------------------------
 ## Data
+## Go to the source of the package and download the *.tar.gz
+## https://www.bioconductor.org/packages/release/workflows/html/methylationArrayAnalysis.html
 
-dataDirectory <- system.file("extdata", package = "methylationArrayAnalysis")
+dataDirectory <- file.path("/Users/kruppajo/Downloads/methylationArrayAnalysis/inst/extdata")
 # list the files
 list.files(dataDirectory, recursive = TRUE)
-
 
 ## -----------------------------------------------------------------------------
 ## Annotation
 
 ann450k <- getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19)
 head(ann450k)
+
+## -----------------------------------------------------------------------------
+## Loading the data
+
+targets <- read.metharray.sheet(dataDirectory, pattern = "SampleSheet.csv")
+targets
+
+rgSet <- read.metharray.exp(targets = targets)
+rgSet

a36afb5faf756bd6989542d6c3e4a1a488fd9d73
Jochen Kruppa <jkruppa@users.noreply.github.com>
2024-04-28 20:47:53 +0200

    added

--- talks/202405_habil_lehrprobe/code/methylation_analysis.R
+++ talks/202405_habil_lehrprobe/code/methylation_analysis.R
@@ -1,1 +1,19 @@
-pacman::p_load(tidyverse)
+## -----------------------------------------------------------------------------
+## CAUTION: The installation of the packages will take a bunch (!) of time... 
+
+pacman::p_load(tidyverse, IlluminaHumanMethylation450kanno.ilmn12.hg19,
+               methylationArrayAnalysis)
+
+## 
+## Data
+
+dataDirectory <- system.file("extdata", package = "methylationArrayAnalysis")
+# list the files
+list.files(dataDirectory, recursive = TRUE)
+
+
+## -----------------------------------------------------------------------------
+## Annotation
+
+ann450k <- getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19)
+head(ann450k)

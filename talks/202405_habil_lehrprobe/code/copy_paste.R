40d14771b1eac99bec6e1f679299c2ddc7fba132
Jochen Kruppa <jkruppa@users.noreply.github.com>
2024-04-30 11:03:02 +0200

    added

---
+++ talks/202405_habil_lehrprobe/code/copy_paste.R
@@ -0,0 +1,134 @@
+detP <- detectionP(rgSet)
+head(detP)
+
+
+# remove poor quality samples
+keep <- colMeans(detP) < 0.05
+rgSet <- rgSet[,keep]
+rgSet
+
+# remove poor quality samples from targets data
+targets <- targets[keep,]
+targets[,1:5]
+
+# remove poor quality samples from detection p-value table
+detP <- detP[,keep]
+dim(detP)
+
+
+# visualise what the data looks like before and after normalisation
+par(mfrow=c(1,2))
+densityPlot(rgSet, sampGroups=targets$Sample_Group,main="Raw", legend=FALSE)
+legend("top", legend = levels(factor(targets$Sample_Group)), 
+       text.col=brewer.pal(8,"Dark2"))
+densityPlot(getBeta(mSetSq), sampGroups=targets$Sample_Group,
+            main="Normalized", legend=FALSE)
+legend("top", legend = levels(factor(targets$Sample_Group)), 
+       text.col=brewer.pal(8,"Dark2"))
+
+
+# MDS plots to look at largest sources of variation
+par(mfrow=c(1,2))
+plotMDS(getM(mSetSq), top=1000, gene.selection="common", 
+        col=pal[factor(targets$Sample_Group)])
+legend("top", legend=levels(factor(targets$Sample_Group)), text.col=pal,
+       bg="white", cex=0.7)
+
+plotMDS(getM(mSetSq), top=1000, gene.selection="common",  
+        col=pal[factor(targets$Sample_Source)])
+legend("top", legend=levels(factor(targets$Sample_Source)), text.col=pal,
+       bg="white", cex=0.7)
+
+# exclude cross reactive probes 
+xReactiveProbes <- read.csv(file=paste(dataDirectory,
+                                       "48639-non-specific-probes-Illumina450k.csv",
+                                       sep="/"), stringsAsFactors=FALSE)
+keep <- !(featureNames(mSetSqFlt) %in% xReactiveProbes$TargetID)
+table(keep)
+
+
+# calculate M-values for statistical analysis
+mVals <- getM(mSetSqFlt)
+head(mVals[,1:5])
+
+bVals <- getBeta(mSetSqFlt)
+head(bVals[,1:5])
+
+# this is the factor of interest
+cellType <- factor(targets$Sample_Group)
+# this is the individual effect that we need to account for
+individual <- factor(targets$Sample_Source) 
+
+# use the above to create a design matrix
+design <- model.matrix(~0+cellType+individual, data=targets)
+colnames(design) <- c(levels(cellType),levels(individual)[-1])
+
+# fit the linear model 
+fit <- lmFit(mVals, design)
+# create a contrast matrix for specific comparisons
+contMatrix <- makeContrasts(naive-rTreg,
+                            naive-act_naive,
+                            rTreg-act_rTreg,
+                            act_naive-act_rTreg,
+                            levels=design)
+contMatrix
+
+
+# fit the contrasts
+fit2 <- contrasts.fit(fit, contMatrix)
+fit2 <- eBayes(fit2)
+
+# look at the numbers of DM CpGs at FDR < 0.05
+summary(decideTests(fit2))
+
+# get the table of results for the first contrast (naive - rTreg)
+ann450kSub <- ann450k[match(rownames(mVals),ann450k$Name),
+                      c(1:4,12:19,24:ncol(ann450k))]
+DMPs <- topTable(fit2, num=Inf, coef=1, genelist=ann450kSub)
+head(DMPs)
+
+## DMR
+
+myAnnotation <- cpg.annotate(object = mVals, datatype = "array", what = "M", 
+                             analysis.type = "differential", design = design, 
+                             contrasts = TRUE, cont.matrix = contMatrix, 
+                             coef = "naive - rTreg", arraytype = "450K")
+
+DMRs <- dmrcate(myAnnotation, lambda=1000, C=2)
+results.ranges <- extractRanges(DMRs)
+results.ranges
+
+
+# set up the grouping variables and colours
+groups <- pal[1:length(unique(targets$Sample_Group))]
+names(groups) <- levels(factor(targets$Sample_Group))
+cols <- groups[as.character(factor(targets$Sample_Group))]
+
+# draw the plot for the top DMR
+par(mfrow=c(1,1))
+DMR.plot(ranges = results.ranges, dmr = 2, CpGs = bVals, phen.col = cols, 
+         what = "Beta", arraytype = "450K", genome = "hg19")
+
+
+## Gene ontology testing
+
+# Get the significant CpG sites at less than 5% FDR
+sigCpGs <- DMPs$Name[DMPs$adj.P.Val<0.05]
+# First 10 significant CpGs
+sigCpGs[1:10]
+
+par(mfrow=c(1,1))
+gst <- gometh(sig.cpg=sigCpGs, all.cpg=all, plot.bias=TRUE)
+
+# Top 10 GO categories
+topGSA(gst, number=10)
+
+
+# load Broad human curated (C2) gene sets
+load(paste(dataDirectory,"human_c2_v5.rdata",sep="/"))
+# perform the gene set test(s)
+gsa <- gsameth(sig.cpg=sigCpGs, all.cpg=all, collection=Hs.c2)
+
+# top 10 gene sets
+topGSA(gsa, number=10)
+
--- talks/202405_habil_lehrprobe/code/methylation_analysis.R
+++ talks/202405_habil_lehrprobe/code/methylation_analysis.R
@@ -26,3 +26,93 @@ targets
 
 rgSet <- read.metharray.exp(targets = targets)
 rgSet
+
+## -----------------------------------------------------------------------------
+## Quality control
+
+detP <- detectionP(rgSet)
+head(detP)
+
+# remove poor quality samples
+keep <- colMeans(detP) < 0.05
+rgSet <- rgSet[,keep]
+rgSet
+
+# remove poor quality samples from targets data
+targets <- targets[keep,]
+targets[,1:5]
+
+# remove poor quality samples from detection p-value table
+detP <- detP[,keep]
+dim(detP)
+
+## -----------------------------------------------------------------------------
+## Normalisation
+
+mSetSq <- preprocessQuantile(rgSet) 
+mSetRaw <- preprocessRaw(rgSet)
+
+# visualise what the data looks like before and after normalisation
+par(mfrow=c(1,2))
+densityPlot(rgSet, sampGroups=targets$Sample_Group,main="Raw", legend=FALSE)
+legend("top", legend = levels(factor(targets$Sample_Group)), 
+       text.col=brewer.pal(8,"Dark2"))
+densityPlot(getBeta(mSetSq), sampGroups=targets$Sample_Group,
+            main="Normalized", legend=FALSE)
+legend("top", legend = levels(factor(targets$Sample_Group)), 
+       text.col=brewer.pal(8,"Dark2"))
+
+## -----------------------------------------------------------------------------
+## Data exploration
+
+# MDS plots to look at largest sources of variation
+par(mfrow=c(1,2))
+plotMDS(getM(mSetSq), top=1000, gene.selection="common", 
+        col=pal[factor(targets$Sample_Group)])
+legend("top", legend=levels(factor(targets$Sample_Group)), text.col=pal,
+       bg="white", cex=0.7)
+
+plotMDS(getM(mSetSq), top=1000, gene.selection="common",  
+        col=pal[factor(targets$Sample_Source)])
+legend("top", legend=levels(factor(targets$Sample_Source)), text.col=pal,
+       bg="white", cex=0.7)
+
+## -----------------------------------------------------------------------------
+## Further filtering...
+
+# ensure probes are in the same order in the mSetSq and detP objects
+detP <- detP[match(featureNames(mSetSq),rownames(detP)),] 
+
+# remove any probes that have failed in one or more samples
+keep <- rowSums(detP < 0.01) == ncol(mSetSq) 
+table(keep)
+
+mSetSqFlt <- mSetSq[keep,]
+mSetSqFlt
+
+# exclude cross reactive probes 
+xReactiveProbes <- read.csv(file=paste(dataDirectory,
+                                       "48639-non-specific-probes-Illumina450k.csv",
+                                       sep="/"), stringsAsFactors=FALSE)
+keep <- !(featureNames(mSetSqFlt) %in% xReactiveProbes$TargetID)
+table(keep)
+
+# remove probes with SNPs at CpG site
+mSetSqFlt <- dropLociWithSnps(mSetSqFlt)
+
+
+## -----------------------------------------------------------------------------
+## Probe-wise differential methylation analysis
+
+
+# calculate M-values for statistical analysis
+mVals <- getM(mSetSqFlt)
+head(mVals[,1:5])
+
+bVals <- getBeta(mSetSqFlt)
+head(bVals[,1:5])
+
+
+
+
+

d2e536d1cb3e60cad3ffac5c54920fad272511fe
Jochen Kruppa <jkruppa@users.noreply.github.com>
2024-04-30 11:23:30 +0200

    added

--- talks/202405_habil_lehrprobe/code/methylation_analysis.R
+++ talks/202405_habil_lehrprobe/code/methylation_analysis.R
@@ -1,7 +1,9 @@
 ## -----------------------------------------------------------------------------
 ## CAUTION: The installation of the packages will take a bunch (!) of time... 
 
-pacman::p_load(tidyverse, IlluminaHumanMethylation450kanno.ilmn12.hg19)
+pacman::p_load(tidyverse, 
+               minfi,
+               IlluminaHumanMethylation450kanno.ilmn12.hg19)
 
 ## -----------------------------------------------------------------------------
 ## Data
@@ -104,7 +106,6 @@ mSetSqFlt <- dropLociWithSnps(mSetSqFlt)
 ## -----------------------------------------------------------------------------
 ## Probe-wise differential methylation analysis
 
-
 # calculate M-values for statistical analysis
 mVals <- getM(mSetSqFlt)
 head(mVals[,1:5])
@@ -112,7 +113,93 @@ head(mVals[,1:5])
 bVals <- getBeta(mSetSqFlt)
 head(bVals[,1:5])
 
+# this is the factor of interest
+cellType <- factor(targets$Sample_Group)
+# this is the individual effect that we need to account for
+individual <- factor(targets$Sample_Source) 
 
+# use the above to create a design matrix
+design <- model.matrix(~0+cellType+individual, data=targets)
+colnames(design) <- c(levels(cellType),levels(individual)[-1])
 
+# fit the linear model 
+fit <- lmFit(mVals, design)
+# create a contrast matrix for specific comparisons
+contMatrix <- makeContrasts(naive-rTreg,
+                            naive-act_naive,
+                            rTreg-act_rTreg,
+                            act_naive-act_rTreg,
+                            levels=design)
+contMatrix
+
+# fit the contrasts
+fit2 <- contrasts.fit(fit, contMatrix)
+fit2 <- eBayes(fit2)
+
+# look at the numbers of DM CpGs at FDR < 0.05
+summary(decideTests(fit2))
+
+# get the table of results for the first contrast (naive - rTreg)
+ann450kSub <- ann450k[match(rownames(mVals),ann450k$Name),
+                      c(1:4,12:19,24:ncol(ann450k))]
+DMPs <- topTable(fit2, num=Inf, coef=1, genelist=ann450kSub)
+head(DMPs)
+
+## -> what is logFC?
+## -> what is AveExpr?
+## -> how to vulcano?
+
+## -----------------------------------------------------------------------------
+## Differential methylation analysis of regions
+
+myAnnotation <- cpg.annotate(object = mVals, datatype = "array", what = "M", 
+                             analysis.type = "differential", design = design, 
+                             contrasts = TRUE, cont.matrix = contMatrix, 
+                             coef = "naive - rTreg", arraytype = "450K")
+
+DMRs <- dmrcate(myAnnotation, lambda=1000, C=2)
+results.ranges <- extractRanges(DMRs)
+results.ranges
+
+# set up the grouping variables and colours
+groups <- pal[1:length(unique(targets$Sample_Group))]
+names(groups) <- levels(factor(targets$Sample_Group))
+cols <- groups[as.character(factor(targets$Sample_Group))]
+
+# draw the plot for the top DMR
+par(mfrow=c(1,1))
+DMR.plot(ranges = results.ranges, dmr = 2, CpGs = bVals, phen.col = cols, 
+         what = "Beta", arraytype = "450K", genome = "hg19")
+
+## -----------------------------------------------------------------------------
+## Customising visualisations of methylation data
+
+## https://www.bioconductor.org/packages/release/workflows/vignettes/methylationArrayAnalysis/inst/doc/methylationArrayAnalysis.html
+
+## -----------------------------------------------------------------------------
+## Gene ontology testing
+
+# Get the significant CpG sites at less than 5% FDR
+sigCpGs <- DMPs$Name[DMPs$adj.P.Val<0.05]
+# First 10 significant CpGs
+sigCpGs[1:10]
+
+par(mfrow=c(1,1))
+gst <- gometh(sig.cpg=sigCpGs, all.cpg=all, plot.bias=TRUE)
+
+# Top 10 GO categories
+topGSA(gst, number=10)
+
+# load Broad human curated (C2) gene sets
+load(paste(dataDirectory,"human_c2_v5.rdata",sep="/"))
+# perform the gene set test(s)
+gsa <- gsameth(sig.cpg=sigCpGs, all.cpg=all, collection=Hs.c2)
+
+# top 10 gene sets
+topGSA(gsa, number=10)
+
+## -----------------------------------------------------------------------------
+## Better visualization
+## https://www.bioconductor.org/packages/devel/bioc/vignettes/MEAL/inst/doc/MEAL.html
 
 

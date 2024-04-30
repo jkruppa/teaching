f92162b0fdd33397351545502b81a55770cca94d
Jochen Kruppa <jkruppa@users.noreply.github.com>
2024-04-30 17:05:13 +0200

    Update methylation_analysis.R

--- talks/202405_habil_lehrprobe/code/methylation_analysis.R
+++ talks/202405_habil_lehrprobe/code/methylation_analysis.R
@@ -1,7 +1,14 @@
 ## -----------------------------------------------------------------------------
 ## CAUTION: The installation of the packages will take a bunch (!) of time... 
 
-pacman::p_load(tidyverse, IlluminaHumanMethylation450kanno.ilmn12.hg19)
+pacman::p_load(tidyverse, 
+               minfi,
+               minfiData,
+               IlluminaHumanMethylation450kanno.ilmn12.hg19,
+               IlluminaHumanMethylation450kmanifest,
+               limma)
+
+options(matrixStats.useNames.NA = "deprecated")
 
 ## -----------------------------------------------------------------------------
 ## Data
@@ -24,5 +31,30 @@ head(ann450k)
 targets <- read.metharray.sheet(dataDirectory, pattern = "SampleSheet.csv")
 targets
 
+## read in the idat files
 rgSet <- read.metharray.exp(targets = targets)
 rgSet
+
+# give the samples descriptive names
+targets$ID <- paste(targets$Sample_Group, targets$Sample_Name, sep=".")
+sampleNames(rgSet) <- targets$ID
+rgSet
+
+## -----------------------------------------------------------------------------
+## Quality control
+detP <- detectionP(rgSet)
+
+## -> okay... what can we do?
+
+## go on wuth QC report
+qcReport(rgSet, sampNames = targets$ID, sampGroups = targets$Sample_Group, 
+         pdf = "qcReport.pdf")
+
+## Normalization
+
+mSetSq <- preprocessQuantile(rgSet) 
+mSetFn <- preprocessFunnorm(rgSet) 
+mSetIl <- preprocessSWAN(rgSet)
+mSetRaw <- preprocessRaw(rgSet)
+
+

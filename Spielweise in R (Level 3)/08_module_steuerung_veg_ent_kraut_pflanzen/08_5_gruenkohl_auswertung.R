f03f56dc131ea366a32a8ba7c29072257b66247f
Jochen Kruppa <jkruppa@users.noreply.github.com>
2024-06-01 14:12:52 +0200

    Update 08_5_gruenkohl_auswertung.R

--- Spielweise in R (Level 3)/08_module_steuerung_veg_ent_kraut_pflanzen/08_5_gruenkohl_auswertung.R
+++ Spielweise in R (Level 3)/08_module_steuerung_veg_ent_kraut_pflanzen/08_5_gruenkohl_auswertung.R
@@ -2,6 +2,9 @@ pacman::p_load(tidyverse, readxl, report, emmeans,
                multcomp, zoo, see,  conflicted)
 conflicts_prefer(dplyr::select)
 conflicts_prefer(dplyr::filter)
+cbb_pal <- c("#E69F00", "#56B4E9", "#009E73", 
+             "#F0E442", "#0072B2", "#D55E00", 
+             "#CC79A7", "#999999", "#000000")
 
 green_tbl <- read_excel("/Users/kruppajo/work/GitHub/teaching/Spielweise in R (Level 3)/08_module_steuerung_veg_ent_kraut_pflanzen/wachstum_gruenkohl.xlsx") %>% 
   mutate(light = as_factor(light),
@@ -63,13 +66,24 @@ height_long_tbl %>%
   ggplot(aes(x = time, y = height,
              color = interaction(variety, light), linetype = interaction(variety, light))) +
   theme_minimal() +
-  geom_point(position = position_dodge(0.9)) +
+  geom_point(position = position_dodge(0.9), show_guide = FALSE) +
   stat_smooth(method = "loess", se = FALSE) +
   labs(color = "name", linetype = "name") +
-  scale_color_manual(name = "name", values = c(1, 3, 4, 5)) +
-  scale_linetype_manual(name = "name", values = c(1, 3, 1, 3)) 
+  scale_color_manual(name = "name", values = cbb_pal[c(1:3, 6)]) +
+  scale_linetype_manual(name = "name", values = c(1, 3, 1, 3)) +
+  theme(legend.key.width = unit(2,"line"))
 
-
+height_long_tbl %>% 
+  ggplot(aes(x = time, y = height, shape = interaction(variety, light),
+             color = interaction(variety, light), linetype = interaction(variety, light))) +
+  theme_minimal() +
+  geom_point(position = position_dodge(0.9), size = 2) +
+  stat_smooth(method = "loess", se = FALSE) +
+  labs(color = "name", linetype = "name") +
+  scale_color_manual(name = "name", values = cbb_pal[c(1:3, 6)]) +
+  scale_linetype_manual(name = "name", values = c(1, 3, 1, 3)) +
+  scale_shape_manual(name = "name", values = c(1:4)) +
+  theme(legend.key.width = unit(2,"line"))
 
 ggplot(data = mydf, aes(x = year, y = mean,
                         color = interaction(treatment, treatment.type),

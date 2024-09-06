bef66c6edd097719d52d0ceb2e41b0d7267b8f52
Jochen Kruppa <jkruppa@users.noreply.github.com>
2024-09-06 10:47:22 +0200

    Create Deskritpive_Statistik.R

---
+++ lecture/Bio_Data_Science/Deskritpive_Statistik.R
@@ -0,0 +1,13 @@
+c(5.7, 8.9, 11.8, 5.6, 9.1, 8.2, 7.6)
+
+jump_length <- c(5.7, 8.9, 11.8, 5.6, 9.1, 8.2, 7.6)
+
+(5.7 + 8.9 + 11.8 + 5.6 + 9.1 + 8.2 + 7.6)/7
+
+sum(jump_length)/7
+
+mean(jump_length)
+
+sum((jump_length - 8.13)^2)/6
+
+var(jump_length)
